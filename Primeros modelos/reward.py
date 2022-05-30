69b80b3fe902f8804dc520d5c09c82c652fa2560
jperezdelcerro <pdcjosefina@gmail.com>
2022-05-30 12:49:08 -0300

    update reward
    delete reward fran

--- Primeros modelos/reward.py
+++ Primeros modelos/reward.py
@@ -3,9 +3,13 @@ import math
 
 
 # Thresholds 
-DIRECTION_THRESHOLD = 10.0
+DIRECTION_THRESHOLD = 4
 SPEED_THRESHOLD = 1
-MAX_SPEED_THRESHOLD = 2
+MAX_SPEED_THRESHOLD = 3
+GOOD_ANGLES_TRACK =  [0, 90, 180 ,270]
+GOOD_ANGLES_HEADING =  [0, 90, 180, -90, -180]
+TOTAL_NUM_STEPS = 300
+STEERING_ANGLES = []
 
 def reward_function(params):
     '''
@@ -16,19 +20,31 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     all_wheels_on_track = params['all_wheels_on_track']
     waypoints = params['waypoints']
+    progress = params['progress']
     speed = params['speed']
+    steering_angle = params['steering_angle']
+    steps = params['steps']
     closest_waypoints = _getClosestWaypoints(params)
     heading = _getHeading(params)
+
+    #saving previous angles
+    if steering_angle:
+        STEERING_ANGLES.append(steering_angle)
     
     
     #Calculate direction 
     track_direction = getTrackDirection(waypoints, closest_waypoints)
+    direction_diff = getDirectionDiff(track_direction, heading) 
     
     # Give higher reward if the car is inside the track
     if all_wheels_on_track:
         # Give higher reward if the car is closer to center line and vice versa
+        reward = 1e-15
         reward = markersPenalty(distance_from_center, track_width)
-        reward = speedPenalty(track_direction, speed, reward)
+        reward = curveSpeedPenalty(direction_diff, speed, reward)
+        reward = straightSpeedPenalty(track_direction, speed, reward)
+        #reward = zigzagPenalty(STEERING_ANGLES) #to do
+        reward = stepsReward(steps, progress, reward)
         
         # Penalize the reward if the difference is too large
         if getDirectionDiff(track_direction, heading) > DIRECTION_THRESHOLD:
@@ -62,27 +78,39 @@ def markersPenalty(distance_from_center, track_width):
     if distance_from_center <= marker * track_width:
       return 1-marker
 
-def speedPenalty(track_direction, speed, reward):
+def curveSpeedPenalty(direction_diff, speed, reward):
+  #if the car isnt going staight, and the speed is 
+    deltaAngle = 5
+    if direction_diff > deltaAngle: #
+        if speed > SPEED_THRESHOLD:
+            reward *= 0.5
+    return reward
 
-  deltaAngle = 5
-  goodAngles =  [0, 90, 180 ,270]
+def straightSpeedPenalty(track_direction, speed, reward):
+    # If the track path is straight and the car is not going top speed, penalize
+    if track_direction in GOOD_ANGLES_TRACK:
+        if speed != MAX_SPEED_THRESHOLD:
+                reward *= 0.5  
+    return reward
 
-  if speed > MAX_SPEED_THRESHOLD: #si se va de la maxima velocidad, lo penalizamos
-      reward *= 0.3
-      return reward
+def zigzagPenalty(track_direction, steering_angle, reward):
+    # if the track path is straight and the car is steering too much, penalize
+     
+    deltaAngle = 3
+    if track_direction in GOOD_ANGLES_TRACK:
+        if abs(steering_angle) - deltaAngle > 0:
+                reward *= 0.5
+             
+    return reward
 
-
-  for angle in goodAngles:
-    if angle - deltaAngle <= track_direction <= angle + deltaAngle: #no dejarlo ir rapido al doblar 
-      if speed <= SPEED_THRESHOLD:
-          if reward:
-            reward += 0.8
-          else:
-            reward = 0.8
-      else:
-        reward *= 0.5
-  
-  return reward
+def stepsReward(steps, progress, reward):
+    # Give additional reward if the car pass every 100 steps faster than expected
+    
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        if reward:
+            reward += 10.0
+        
+    return reward
 
 def _getClosestWaypoints(params):
     # closest_waypoints
